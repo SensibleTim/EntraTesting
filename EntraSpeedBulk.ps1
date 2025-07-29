76d470651578c79dd317ccd34c2b5a274e9b8109
Tim Springston <130946153+SensibleTim@users.noreply.github.com>
2025-07-29 15:46:55 -0500

    Update EntraSpeedBulk.ps1
    Added export CSVs for users and groups list to enable scripting against the newly created objects.
    
    Added functionality to automatically add authentication methods during user creation.

--- EntraSpeedBulk.ps1
+++ EntraSpeedBulk.ps1
@@ -1,12 +1,13 @@
-﻿#************************************************
+#************************************************
 # EntraSpeedBulk.ps1
-# Version 1.0
-# Date: 7-11-2025
+# Version 1.1
+# Date: 7-29-2025
 # Author: Tim Springston
-# Description: This script can be used to add a large number of Entra ID objects to a tenant quickly. 
-#  The use case for this script is to simply add a large number of objects for operational and recovery scenario testing. 
-#  This script will create an admin-specified number applications, service principals, users, and groups. 
-#  Conditional Access policies have a default max of 200 per tenant. The script will add as many as possible while leaving room for 10 additional new policies to be created.
+# Description: This script can be used to add a large number of Entra ID objects to a tenant quickly.
+#  The script is handy for quickly and easily adding a large number of objects for operational and recovery scenario testing.
+#  This script will create an admin-specified number applications, service principals, users, and groups.
+#  Conditional Access policies have a default max of 200 per tenant, and the script can add as many as possible
+#      while leaving room for 10 additional new policies to be created for security needs.
 #
 #  This script relies on Windows 11 cloud integration for authentication and the Microsoft.Entra PowerShell modules.
 #   Install-Module -Name Microsoft.Entra -Repository PSGallery -Scope CurrentUser -Force -AllowClobber ##
@@ -21,6 +22,7 @@ param (
      [int]$NumberofAppsandSPs
      )
 $Tenant = Get-EntraTenantDetail
+
 $A = 1
 For ($A; $A -le $NumberofAppsandSPs; $A++)
 {
@@ -87,30 +89,82 @@ param (
         [int]$NumberofUsers
        )
 $Tenant = Get-EntraTenantDetail
+[array]$CreatedUsers = @()
+
+#Create Users
 For ($U; $U -le $NumberofUsers; $U++)
-{
-$GUID = New-GUID
-$Username = $GUID.Guid.Split('-')[0]
-$UPN =  "Test_User_" + $Username + "@" + $Tenant.VerifiedDomains[0].name
-$PasswordProfile = @{Password = new-guid }
-New-EntraUser -AccountEnabled $true -DisplayName $Username -UserPrincipalName $UPN `
--PasswordProfile $PasswordProfile -MailNickName $Username -InformationAction SilentlyContinue
-Write-Host $UPN
-$Username = $null}
+    {
+    #Create user
+    $GUID = New-GUID
+    $Username = $GUID.Guid.Split('-')[0]
+    $UPN =  "Test_User_" + $Username + "@" + $Tenant.VerifiedDomains[0].name
+    $PasswordProfile = @{Password = new-guid }
+    New-EntraUser -AccountEnabled $true -DisplayName $Username -UserPrincipalName $UPN `
+    -PasswordProfile $PasswordProfile -MailNickName $Username -InformationAction SilentlyContinue
+    Write-Host "Created new user $UPN."
+    $CreatedUsers += $UPN.ToString()
+    $Username = $null
+    $UPN = $null
+    }
+
+Start-sleep -Seconds 60 #take a break for eventual consistency
+
+#add email methods to users
+Foreach ($User in $CreatedUsers)
+    {
+    $emailAddress = @{emailAddress = $User}
+    $URI = "https://graph.microsoft.com/v1.0/users/" + $User + "/authentication/emailMethods"
+    Invoke-MgGraphRequest -Method POST  -Uri $URI -Body $emailAddress
+    Write-host "Adding email authentication method to user $User."
+    }
+
+#Put UPN array into PS object of strings
+$UPNPSObj = foreach ($item in $CreatedUsers) {
+   [PSCustomObject]@{
+   userPrincipalname = $item
+   }
+ }
+
+#Export to XLSX
+$Date = Get-Date -Format "yyyy-MM-dd"
+[string]$Datestring = $Date
+$FilePath = 'C:\BulkRestoreInfo\UserList' + $Datestring +  '.csv'
+$UPNPSObj | Export-Csv  -Path $Filepath -NoTypeInformation
+
 }
+
 
 function CreateManyGroups {
 param (
-    [int]$NumberofGroups
+    [int]$NumberofGroups,
+    [int]$StartNumber
     )
+$G = $StartNumber
+[array]$CreatedGroups = @()
+
 For ($G; $G -le $NumberofGroups; $G++)
 {
 $GUID = New-GUID
-$Groupname =  "Test_Group_" + $GUID.Guid.Split('-')[0]
+$Group = $GUID.Guid.Split('-')[0]
+$Groupname = "Test_Group_" + $Group
 New-EntraGroup -Description "Bulk Added Group" -DisplayName $Groupname -SecurityEnabled $True `
--MailEnabled $false -MailNickName $Groupname -InformationAction SilentlyContinue
+-MailEnabled $false -MailNickName $Groupname
 Write-Host $Groupname
-$Groupname = $null}
+$CreatedGroups += $Groupname.ToString()
+$Groupname = $null
+}
+#Put UPN array into PS object of strings
+$GroupsObj = foreach ($item in $CreatedGroups) {
+   [PSCustomObject]@{
+   displayName = $item
+   }
+ }
+ #Export to XLSX
+$Date = Get-Date -Format "yyyy-MM-dd"
+[string]$Datestring = $Date
+$FilePath = 'C:\BulkRestoreInfo\GroupList' + $Datestring +  '.csv'
+$GroupsObj | Export-Csv  -Path $Filepath -NoTypeInformation
+
 
 }
 
@@ -121,25 +175,34 @@ $Groupname = $null}
 cls
 Import-Module Microsoft.Entra
 
-Write-host "Welcome to the Entra Speed Bulk PowerShell script. The script uses the Microsoft.Entra and Microsoft Graph CA policy modules."
+Write-host "Welcome to the Entra Speed Bulk PowerShell script. The script uses the Microsoft.Entra and Microsoft Graph policy modules."
 Write-host " This script provides automation to quickly add objects to an Entra ID test tenant. You will be prompted for how many of each object to create. The added objects are simply filler-they will not be usable for login or other scenarios outside of recovery testing."
+Write-host "Information about the objects created will be exported to the C:\BulkRestoreInfo folder."
+
+$folderPath = 'C:\BulkRestoreInfo'  # Specify the folder path
+# Check if the folder does not exist
+if (-not (Test-Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory -InformationAction SilentlyContinue  # Create the folder
+}
+
+
 $Domain = Read-Host "Enter the domain name of your tenant"
 
 Connect-Entra -TenantId $Domain -NoWelcome
 
-[int]$NumberofAppsandSPs = Read-Host "Enter the number of Application and Service Principal objects to create."
+[int]$NumberofAppsandSPs = Read-Host "Enter the number of Application and Service Principal objects to create"
 CreateManyAppsandSPs $NumberofAppsandSPs
 
-$CreatePolicies = Read-Host "Create CA policies? Enter Yes or No."
+$CreatePolicies = Read-Host "Create CA policies? Enter Yes or No"
 If ($CreatePolicies -match "Yes")
     {
     Write-Host "Creating Conditional Access policy objects. All policies are created in Report-only mode."
     CreateManyCAPolicies
     }
 
-[int]$NumberofUsers = Read-Host "Enter the number of user objects to create."
+[int]$NumberofUsers = Read-Host "Enter the number of user objects to create"
 CreateManyUsers $NumberofUsers
 
-[int]$NumberofGroups = Read-Host "Enter the number of group objects to create."
+[int]$NumberofGroups = Read-Host "Enter the number of group objects to create"
 CreateManyGroups $NumberofGroups
  
